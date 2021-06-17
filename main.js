a08ffe171d33379a1ee125ada3c7ac7bac8f312e
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-06-17 19:43:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ function add() {
     background.src=background_Image;
 
     car1_img=new Image();
-    car1_img.onload=drawCar;
+    car1_img.onload=drawCar1;
     car1_img.src=car1Image;
 
     car2_img=new Image();
-    car2_img.onload=drawCar;
+    car2_img.onload=drawCar2;
     car2_img.src=car2Image;
 }
 
@@ -34,8 +34,12 @@ function drawBackground() {
     ctx.drawImage(background,0,0,canvas.width,canvas.height);
 }
 
-function drawCar() {
+function drawCar1() {
+
     ctx.drawImage(car1_img,car1X,car1Y,car1Height,car1Width);
+}
+
+function drawCar2() {
 
     ctx.drawImage(car2_img,car2X,car2Y,car2Height,car2Width);
 }
@@ -89,3 +93,81 @@ function mykeyDown(e) {
         console.log("Key d is Pressed");
     }
 }
+
+//Car1 code ⬇⬇
+function car1_up() {
+    if (car1Y >= 0) {
+        car1Y= car1Y-10;
+        console.log(car1Y);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car1_down() {
+    if (car1Y >= 450) {
+        car1Y= car1Y+10;
+        console.log(car1Y);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car1_left() {
+    if (car1X >= 0) {
+        car1X = car1X-10;
+        console.log(car1X);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car1_right() {
+    if (car1X >= 700) {
+        car1X = car1X+10;
+        console.log(car1X);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+
+
+//Car2 code ⬇⬇
+function car2_up() {
+    if (car2Y >= 0) {
+        car2Y= car2Y-10;
+        console.log(car2Y);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car2_down() {
+    if (car2Y >= 450) {
+        car2Y= car2Y+10;
+        console.log(car2Y);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car2_left() {
+    if (car2X >= 0) {
+        car2X = car2X-10;
+        console.log(car2X);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+function car2_right() {
+    if (car2X >= 700) {
+        car2X = car2X+10;
+        console.log(car2X);
+        drawBackground();
+        drawCar1();
+        drawCar2();
+    }
+}
+
